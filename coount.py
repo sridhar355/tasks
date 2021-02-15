f4a55e20431960cc3778d9b1c0320ede52f509bb
sridhar <54585961+sridhar355@users.noreply.github.com>
2021-02-15 20:53:53 +0530

    Add files via upload

---
+++ coount.py
@@ -0,0 +1,21 @@
+str1=input()
+opstr=""
+dup=[]
+strlst=(list(str1))
+n=len(strlst)
+visit=[]
+for i in range(n):
+    visit.append(0)
+for j in range(n):
+    count=0
+    for k in range(j,n):
+        if ((visit[k]==0)and(strlst[j]==strlst[k])):
+            if strlst[j] not in dup:
+                dup.append(strlst[j])
+                opstr=opstr+strlst[j]
+            visit[k]=1 
+            count=count+1
+    if(count>0):
+        opstr=opstr+str(count)
+
+print(opstr)
